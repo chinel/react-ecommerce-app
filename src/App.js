45591dc19972a491da701b71f3bab41d20f523a6
nelo <nelokwaz@gmail.com>
2020-01-27 16:06:25 +0100

    updated the shop page dividing the shop page into components and adding each component specific stlyings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shopPage/shop.component';
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <div>
       <Switch>
       <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop" component={ShopPage}/>
       </Switch>
       
     </div>
---
+++ src/components/collection-preview/collection-preview.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+
+const CollectionPreview = ({ title, items}) => (
+    <div className="collection-preview">
+        <h1 className="title">
+            {title.toUpperCase()}
+        </h1>
+        <div className="preview">
+            {
+                items
+                .filter((item, index) => (index < 4))//this allows us to pull out only 4 each items each for each collection
+                .map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} {...otherItemProps}/>
+                ))
+            }
+        </div>
+
+    </div>
+)
+
+
+export default CollectionPreview;
