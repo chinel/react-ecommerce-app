563553b1fcccb48e5c918bf1e3ca8b0a70240f46
nelo <nelokwaz@gmail.com>
2020-02-14 07:33:18 +0100

    moved the user constants to a separate file

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,3 +1,5 @@
+import { userActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
     currentUser: null
 }
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case userActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
