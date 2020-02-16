83eef06dd815cf7028dfd6c278802528edf0841c
nelo <nelokwaz@gmail.com>
2020-02-16 08:07:19 +0100

    added redux persist and setup up some configuration to persist the data from redux state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from '../redux/user/user-reducer';
 import cartReducer from './cart/cart-reducer';
 
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', //this is to state which reducer we want to persist,
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+});
+
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {  persistStore } from 'redux-persist';
 
 
 const middlewares = [logger];
 
 
-const store =  createStore(rootReducer, applyMiddleware(...middlewares));
+export const store =  createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store, persistor}; // we have to export default here incase we need to make reference to the unpersisted store
