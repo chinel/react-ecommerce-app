6648a23a8a1bc4a36890ffc4db8fb4e82ac27cf8
nelo <nelokwaz@gmail.com>
2020-02-18 11:36:58 +0100

    create a collection overview to hold the collection preview component

---
+++ src/components/collections-overview/collections-overview.component.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import "./collections-overview.styles.scss";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shopPage/shop.component.js
+++ src/pages/shopPage/shop.component.js
@@ -1,25 +1,17 @@
 import  React  from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+
 
 const ShopPage = ({collections}) =>  (
         
             <div className="shop-page">
-
-                {collections
-                .map(({id, ...otherCollectionProps}) =>(
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
-            </div>
+                <CollectionsOverview/>
+                </div>
         )
     
 
 
-const mapStateToProps = state => createStructuredSelector({
-    collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
 
