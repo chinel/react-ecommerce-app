e2e85360e87fd5e58cc5f590c33de2a77b0aa457
nelo <nelokwaz@gmail.com>
2020-02-19 18:38:32 +0100

    optimized the retreival of shop data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -19,6 +19,6 @@ export const selectCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    (collections) => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    (collections) => collections[collectionUrlParam]
 )
 
