ea1e173cbea5c9063dd8bce31b31a4771a9fe49c
nelo <nelokwaz@gmail.com>
2020-03-14 13:00:04 +0100

    updated the shop reducer to handle the different states when making the Api call

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,16 +1,28 @@
 import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-      case ShopActionTypes.UPDATE_COLLECTIONS:
+
+      case ShopActionTypes.FETCH_COLLECTIONS_START:
+        return{
+          isFetching: true
+        } 
+      case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
          return {
             ...state,
             collections: action.payload
          }
+      case ShopActionTypes.FETCH_COLLECTIONS_ERROR:
+        return{
+          isFetching: false,
+          errorMessage: action.payload
+        }   
       default:
           return state;
   }
