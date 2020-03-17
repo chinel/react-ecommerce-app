0d6be31c34ce27f7eef562684614a8ccc037a99f
nelo <nelokwaz@gmail.com>
2020-03-17 18:29:54 +0100

    converted the shop action using thunk to a saga action

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,12 +1,35 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put} from "redux-saga/effects";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
 
-import ShopActionTypes from './shop.types';
+import ShopActionTypes from "./shop.types";
+import { fetchCollectionsSuccess, fetchCollectionsError } from "./shop.actions";
 
+export function* fetchCollectionsAsync() {
+  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionMap));
+  } catch (error) {
+      yield put(fetchCollectionsError(error.message));
+  }
 
-export function* fetchCollectionsAsync(){
-    yield console.log("I am fired");
+  /*  collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsError(error.message))); */
 }
 
-export function* fetchCollectionStart(){
-    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync); 
+export function* fetchCollectionStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
 }
