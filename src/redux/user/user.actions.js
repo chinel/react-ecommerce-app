488c8d27ec1c604d4145f51f1a8ad636262fb2c8
nelo <nelokwaz@gmail.com>
2020-03-20 19:57:16 +0100

    refactored the codebase to use a more promise pattern by creating more user types and more user actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,3 +4,36 @@ export const setCurrentUser = (user) => ({
     type: userActionTypes.SET_CURRENT_USER,
     payload: user
 })
+
+export const googleSignInStart = () => ({
+    type: userActionTypes.GOOGLE_SIGN_IN_START
+})
+
+
+export const googleSignInSuccess = (user) => ({
+    type: userActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user
+})
+
+
+export const googleSignInError = (error) => ({
+    type: userActionTypes.GOOGLE_SIGN_IN_ERROR,
+    payload: error
+})
+
+export const emailSignInStart = (emailAndPassword) => ({
+    type: userActionTypes.EMAIL_SIGN_IN_START,
+    payload:emailAndPassword
+})
+
+
+export const emailSignInSuccess = (user) => ({
+    type: userActionTypes.EMAIL_SIGN_IN_SUCCESS,
+    payload: user
+})
+
+
+export const emailSignInError = (error) => ({
+    type: userActionTypes.EMAIL_SIGN_IN_ERROR,
+    payload: error
+})
