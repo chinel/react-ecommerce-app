80f5c71f5bbf28a0df1ede4e3392c40e2f3c5efc
nelo <nelokwaz@gmail.com>
2020-03-22 09:11:08 +0100

    added the clear cart listener to the cart reducer

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -24,11 +24,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
-         case cartActionTypes.REMOVE_ITEM:
+        case cartActionTypes.REMOVE_ITEM:
              return{
                  ...state,
                  cartItems: removeItemFromCart(state.cartItems, action.payload)
-             }   
+             } 
+        case cartActionTypes.CLEAR_CART:
+            return{
+                ...state,
+                cartItems:[]
+            }
         default:
             return state;
     }
