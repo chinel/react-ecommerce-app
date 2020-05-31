986ebfcba35ce1c1e4cf26e3d9a429735bd8126e
nelo <nelokwaz@gmail.com>
2020-05-31 06:59:19 +0100

    added the axios library and created frontend logic using axios to make backend request

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
 //Note before we can process payment in stripe, stripe want the prices to be in cents so if you have dollars you have to convert to cents by multipying by 100
 const priceForStripe = price * 100;
 const publishableKey  = 'pk_test_8iTnEuniHDHAYKS6y9QrSyJE';
 
-const OnToken = token =>{
-    console.log(token);
-    alert("Payment successful");
+const OnToken = token => {
+   axios({
+       url: 'payment',
+       method: 'post',
+       data:{
+           amount: priceForStripe,
+           token
+       }
+   })
+       .then(response => {
+       alert('Payment successful');
+       }).catch(error => {
+      console.log('Payment error ', JSON.parse(error));
+      alert('There was an issue with your payment. Please make sure you use the provided credit card');
+   })
 }
 
 return(
