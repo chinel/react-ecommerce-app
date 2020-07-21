0f123dc994d5c0bf1f044250b328a33f39e5c5d5
nelo <nelokwaz@gmail.com>
2020-07-21 11:22:30 +0100

    made the body responsive

--- client/src/global.styles.jsx
+++ client/src/global.styles.jsx
@@ -4,6 +4,11 @@ export const GlobalStyle =  createGlobalStyle`
 body{
     font-family: 'Open Sans Condensed', sans-serif;
     padding: 20px 60px;
+    
+    
+    @media screen and (max-width: 800px){
+    padding: 10px;
+    }
 }
 
 a{
