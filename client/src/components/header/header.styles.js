25d99e45bb179258589be7174d303f4279f21210
nelo <nelokwaz@gmail.com>
2020-07-21 11:35:23 +0100

    made the header component responsive

--- client/src/components/header/header.styles.js
+++ client/src/components/header/header.styles.js
@@ -12,12 +12,23 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+  
+  @media screen and (max-width: 800px){
+  padding: 10px;
+  height: 60px;
+  margin-bottom: 20px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+  
+  @media screen and (max-width: 800px){
+   width: 50px;
+   padding: 0px;
+  }
 `;
 
 export const OptionsContainer = styled.div`
@@ -26,6 +37,10 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  
+  @media screen and  (max-width: 800px){
+    width: 80%;
+  }
 `;
 
 export const OptionDiv = styled.div`
