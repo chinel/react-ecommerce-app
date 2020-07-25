0afaeea211f3ff4825d07006fcbefe0f25a8385d
nelo <nelokwaz@gmail.com>
2020-07-25 19:41:02 +0100

    implemented code splitting in the shop component page

--- client/src/pages/shopPage/shop.component.js
+++ client/src/pages/shopPage/shop.component.js
@@ -1,10 +1,11 @@
-import React, {useEffect} from "react";
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import React, {lazy, useEffect, Suspense} from "react";
 import { Route } from "react-router-dom";
 import { connect} from 'react-redux';
 import {fetchCollectionsStart} from '../../redux/shop/shop.actions';
-import CollectionPageContainer from "../collection/collection.container";
+import Spinner from "../../components/spinner/spinner.component";
 
+const CollectionsOverviewContainer = lazy(() => import("../../components/collections-overview/collections-overview.container"));
+const CollectionPageContainer = lazy(() => import("../collection/collection.container"));
 
 
 const ShopPage = ({fetchCollectionsStart, match}) =>  {
@@ -16,11 +17,13 @@ const ShopPage = ({fetchCollectionsStart, match}) =>  {
     
     return (
       <div className="shop-page">
+         <Suspense fallback={<Spinner/>}>
         <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}/>
         <Route
           path={`${match.path}/:collectionId`}
           component={CollectionPageContainer}
         />
+         </Suspense>
       </div>
     );
   }
