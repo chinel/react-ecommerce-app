e7b22a5618548cdf6562fd44d6580f4f09b5a84a
nelo <nelokwaz@gmail.com>
2020-07-25 19:35:45 +0100

    converted the withspinner higher order component to a normal component and used it as a fallback for the Suspense component

---
+++ client/src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SpinnerContainer, SpinnerOverlay} from './spinner.styles';
+
+
+const Spinner = () => (
+    <SpinnerContainer>
+        <SpinnerOverlay/>
+    </SpinnerContainer>
+)
+
+export default  Spinner;
--- client/src/components/with-spinner/with-spinner.component.js
+++ client/src/components/with-spinner/with-spinner.component.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
-import {SpinnerContainer, SpinnerOverlay} from './with-spinner.styles';
+import Spinner from "../spinner/spinner.component";
 
 const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => {
     return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer/>
-        </SpinnerOverlay>
+        <Spinner/>
+
     ):
     (<WrappedComponent {...otherProps}/>);
 }
