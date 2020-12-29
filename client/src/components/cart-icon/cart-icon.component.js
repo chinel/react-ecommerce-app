37c1729c22ccba0857bf09137875545d65d47bdb
nelo <nelokwaz@gmail.com>
2020-12-29 07:41:06 +0100

    made some updates to the app

--- client/src/components/cart-icon/cart-icon.component.js
+++ client/src/components/cart-icon/cart-icon.component.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-
-import {ReactComponent as ShoppingIcon} from '../../assets/11.2 shopping-bag.svg';
+import shoppingIcon from "../../assets/11.2-shopping-bag.svg";
 import "./cart-icon.styles.scss";
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
-import { createStructuredSelector } from 'reselect';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
 
 //We are destructuring toggleCartHidden off our properties
-const CartIcon = ({toggleCartHidden, itemCount}) => (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-       <ShoppingIcon className="shopping-icon"/>
-<span className="item-count">{itemCount}</span>
-    </div>
-)
-
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <img className="shopping-icon" src={shoppingIcon} />
+    <span className="item-count">{itemCount}</span>
+  </div>
+);
 
 //this gets a dispatch and we would call it toggleCartHidden
 //this will just be a function that triggers the dispatch of toggleCartHidden Action method from the cart action
 const mapDispatchToProps = (dispatch) => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
 
 const mapStateToProps = createStructuredSelector({
-    //Note: reduce javascript function allows us to reduce an array to a single value and here we also want to accumulate all the quantity values in the cart items array
-    itemCount: selectCartItemsCount //cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0) //here we passed in 0 as the initial accumulated value
-})
+  //Note: reduce javascript function allows us to reduce an array to a single value and here we also want to accumulate all the quantity values in the cart items array
+  itemCount: selectCartItemsCount, //cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0) //here we passed in 0 as the initial accumulated value
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
-
--- client/src/components/header/header.component.js
+++ client/src/components/header/header.component.js
@@ -1,46 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo} from '../../assets/crown.svg';
-import { auth } from '../../firebase/firebase.utils';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
-import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv} from './header.styles.js';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { createStructuredSelector } from 'reselect';
-import { signOutStart } from '../../redux/user/user.actions'
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+  OptionDiv,
+} from "./header.styles.js";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import { signOutStart } from "../../redux/user/user.actions";
 
-
-const Header = ({currentUser, hidden, signOutStart}) => (
-<HeaderContainer>
+const Header = ({ currentUser, hidden, signOutStart }) => (
+  <HeaderContainer>
     <LogoContainer to="/">
-     <Logo className="logo"/>
+      <img src={logo} className="logo" />
     </LogoContainer>
     <OptionsContainer>
-    <OptionLink to="/shop">SHOP</OptionLink>
-    <OptionLink to="/shop">CONTACT</OptionLink>
-    {currentUser ? <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv> : <OptionLink to="/signin">SIGN IN</OptionLink>}
-    <CartIcon/>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/shop">CONTACT</OptionLink>
+      {currentUser ? (
+        <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
+      ) : (
+        <OptionLink to="/signin">SIGN IN</OptionLink>
+      )}
+      <CartIcon />
     </OptionsContainer>
-    {hidden && <CartDropdown/> }
-</HeaderContainer>
-)
-
+    {hidden && <CartDropdown />}
+  </HeaderContainer>
+);
 
 //To destructure  nested values of our state for instance
 //There are other reducers we would also want to get other reducers props as well
 
-
-
 const mapStateToProps = createStructuredSelector({
-    currentUser:selectCurrentUser,
-    hidden: selectCartHidden
-})
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
+});
 
 const mapDispatchToProps = (dispatch) => ({
-    signOutStart: () => dispatch(signOutStart())
-})
+  signOutStart: () => dispatch(signOutStart()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
